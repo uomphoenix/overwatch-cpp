5d8f06e7c1083af5f4c4182aad725c21884891e1
Prithu Parker <prithu@iinet.net.au>
2015-09-17 03:08:06 +1000

    Now implement run() and stop() in the base Container class. run() is intended to be called in a thread

--- src/CameraContainer.cpp
+++ src/CameraContainer.cpp
@@ -2,10 +2,31 @@
 
 CameraContainer::CameraContainer()
 {
-    //ctor
+    running = false;
+
+    latest_ftime = 0;
+    prev_ftime = 0;
 }
 
 CameraContainer::~CameraContainer()
 {
     //dtor
+}
+
+void CameraContainer::run()
+{
+    if (running)
+        return;
+
+    running = true;
+
+    while (running)
+    {
+        getNextFrame();
+    }
+}
+
+void CameraContainer::stop()
+{
+    running = false;
 }
